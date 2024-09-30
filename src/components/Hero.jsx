a1c1169142a2148e91d3a8d3389f425241c7758c
Akhil Kumar <akhil.se2024@gmail.com>
2024-09-30 06:50:07 +0530

    Created Navbar

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='w-full nav-height bg-black relative'>
+    <div className='h-5/6 w-full flex-center flex-col'></div>
+    <p className='hero-title'>Iphone 15 pro</p>
+
+    </section>
+  )
+}
+
+export default Hero
